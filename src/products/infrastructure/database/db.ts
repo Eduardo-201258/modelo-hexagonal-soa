2776bb30b31bf2861c0cdadecaf08c25250cf62b
203445 <203445@ids.upchiapas.edu.mx>
2023-05-28 21:15:48 -0600

    update database and main

--- src/products/infrastructure/database/db.ts
+++ src/products/infrastructure/database/db.ts
@@ -2,19 +2,19 @@ import { Pool } from "pg";
 
 export const pool = new Pool({
   user: "postgres",
-  password: "alejandra",
+  password: "jakakak",
   host: "localhost",
-  database: "productos",
+  database: "producmdsjtos",
   port: 5432,
 });
 
-async function checkDatabaseConnection() {
-  try {
-    const client = await pool.connect();
-    console.log("Conexión a la base de datos establecida correctamente");
-    client.release(); // Liberar el cliente después de la validación
-  } catch (error) {
-    console.error("Error al conectar a la base de datos:", error);
-  }
-}
-checkDatabaseConnection();
+// async function checkDatabaseConnection() {
+//   try {
+//     const client = await pool.connect();
+//     console.log("Conexión a la base de datos establecida correctamente");
+//     client.release(); // Liberar el cliente después de la validación
+//   } catch (error) {
+//     console.error("Error al conectar a la base de datos:", error);
+//   }
+// }
+// checkDatabaseConnection();
